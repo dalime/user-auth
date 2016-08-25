9e8c262e89fad27d6a64eca6df41b9e0fabb5132
duhdanube <Danny.B.Lim@gmail.com>
2016-08-25 14:48:16 -0700

    Signed as user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import UserStore from '../stores/UserStore';
 
 export default class Navbar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      profile: UserStore.get()
+    }
+
+    this._onChange = this._onChange.bind(this);
+  }
+
+  componentDidMount() {
+    UserStore.startListening(this._onChange);
+  }
+
+  componentWillUnmount() {
+    UserStore.stopListening(this._onChange);
+  }
+
+  _onChange() {
+    this.setState({
+      profile: UserStore.get()
+    });
+  }
+
   render() {
+    let { profile } = this.state;
+
+    let Welcome = <p></p>
+
+    if (profile) {
+      Welcome = <p className="navbar-text">Signed in as {profile.username}</p>
+    }
+
     return (
       <div>
       <nav className="navbar navbar-default navbar-static-top">
@@ -17,6 +50,7 @@ export default class Navbar extends Component {
             <Link className="navbar-brand" to="/">React App</Link>
           </div>
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            {Welcome}
             <ul className="nav navbar-nav navbar-right">
               <li><Link to="/">Home</Link></li>
               <li><Link to="/register">Register</Link></li>
